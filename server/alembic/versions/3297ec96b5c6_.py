b88892cfe069415ae7a65e9b4157bf67d22121a6
Igor Beschastnov <beschastnovigor@gmail.com>
2020-06-20 02:53:11 +0300

    fix

---
+++ server/alembic/versions/3297ec96b5c6_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3297ec96b5c6
+Revises: 9a0dd7bca5ef, 2afccf60f35c
+Create Date: 2020-06-19 23:52:50.063423
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3297ec96b5c6'
+down_revision = ('9a0dd7bca5ef', '2afccf60f35c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
