67502b90294c01c517d31e7f2df4ea9d03bde8cc
Igor Beschastnov <beschastnovigor@gmail.com>
2020-06-20 12:53:42 +0300

    ы

--- server/services/schemas.py
+++ server/services/schemas.py
@@ -7,8 +7,3 @@ class ErrorResponse(BaseModel):
     ok: bool = False
     error: Optional[Union[str, Dict, List]] = 'Unknown error'
     error_code: str = 'ERROR'
-
-
-class ORMModel(BaseModel):  # inherit all models from this one
-    class Config:
-        orm_mode = True
